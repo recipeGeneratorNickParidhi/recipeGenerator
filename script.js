4bf8c205ea3688bd6229e57f28b1f7eabe3f38a8
paridhishah96 <paridhishah96@gmail.com>
2022-07-13 22:55:25 -0400

    completed some more styling and media queries, also organised the JS a bit

--- script.js
+++ script.js
@@ -2,27 +2,73 @@
 recipesApp = {};
 
 // Building Array for Cuising Selection
-recipesApp.cuisineOptions = ['italian', 'british', 'chinese']
-
+recipesApp.cuisineOptions = [
+    "African",
+    "American",
+    "British",
+    "Cajun",
+    "Caribbean",
+    "Chinese",
+    "Eastern European",
+    "European",
+    "French",
+    "German",
+    "Greek",
+    "Indian",
+    "Irish",
+    "Italian",
+    "Japanese",
+    "Jewish",
+    "Korean",
+    "Latin American",
+    "Mediterranean",
+    "Mexican",
+    "Middle Eastern",
+    "Nordic",
+    "Southern",
+    "Spanish",
+    "Thai",
+    "Vietnamese",
+]
 // Querying Select from HTML and saving as variable
 recipesApp.select = document.querySelector('select')
+recipesApp.populateCuisineSelect = function (cuisinesArray){
+    // using a forEach loop to populate the select
+    cuisinesArray.forEach((option) => {
+        const newOption = document.createElement('option');
+        newOption.value = option;
+        newOption.textContent = option;
+        recipesApp.select.appendChild(newOption);
+    })
+}
 
-// using a forEach loop to populate the select
-recipesApp.cuisineOptions.forEach((data) => {
-    const newOption = document.createElement('option')
+// galleryApp.apiUrl = "https://api.unsplash.com/photos";
+// galleryApp.apiKey = "Qi9SDpY5MEDJMQvbKtLUlhrGYl4F5a_zSjRWn4e01z8";
 
-    newOption.value = data
-    newOption.textContent = data
-
-    recipesApp.select.appendChild(newOption)
-
-})
-
+// create a method (AKA function on the app object) which requests information from the API
+// logs it to the console
+// galleryApp.getPhotos = function (){
+//   const url = new URL(galleryApp.apiUrl);
+//   url.search = new URLSearchParams({
+//     client_id: galleryApp.apiKey
+//   })
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((jsonResponse) => {
+//       console.log(jsonResponse);
+//       galleryApp.displayPhotos(jsonResponse);
+//     })
+// }
 // Adding event listener to the select to listen for users selection and saving that to a variable
-recipesApp.select.addEventListener('change', function () {
+recipesApp.select.addEventListener('change', function (event) {
+    event.preventDefault()
     const selectedCusine = this.value
     recipesApp.apiCall(selectedCusine)
 })
+
+
 
 // Making api call based on user selection
 recipesApp.apiCall = function (selectedCusine) {
@@ -61,3 +107,11 @@ recipesApp.displaydata = function (recipesData) {
     // outerDiv.appendChild(imageItem);
 
 }
+
+recipesApp.init = function(){
+    recipesApp.populateCuisineSelect(recipesApp.cuisineOptions);
+}
+
+
+// Call the init method to kickstart the app upon page load
+recipesApp.init();
