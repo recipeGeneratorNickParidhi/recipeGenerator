39858b0d47273453897d2a7bb5520b7fb9bb15e3
paridhishah96 <paridhishah96@gmail.com>
2022-07-19 15:34:52 -0400

    merged with modules and hamburger menu

--- main.js/script.js
+++ main.js/script.js
@@ -45,11 +45,27 @@ recipesApp.userQuery = function(){
     })
 }
 
+recipesApp.burgerMenu = function() {
+    document.querySelector('#menuButton').addEventListener('click', function () {
+        document.querySelector('#menu').classList.toggle('showMenu');
+        document.querySelector('#menuButton').classList.add('hidden');
+        document.querySelector('#closeButton').classList.remove('hidden');
+    })
+    document.querySelector('#closeButton').addEventListener('click', function () {
+        document.querySelector('#menu').classList.toggle('showMenu');
+        document.querySelector('#menuButton').classList.remove('hidden');
+        document.querySelector('#closeButton').classList.add('hidden');
+    })
+
+}
+
 // Init
 recipesApp.init = function(){
+    recipesApp.burgerMenu();
     populateSelect(recipesApp.cuisineOptions);
     recipesApp.userQuery();
     cardListener();
+
 }
 // Call the init method to kickstart the app upon page load
 recipesApp.init();
